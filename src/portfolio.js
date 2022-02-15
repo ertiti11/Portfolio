86378f237228cecfe39963f6d95d2b91a05022c2
ertiti11 <vatitiprieto11@gmail.com>
2022-02-15 15:08:50 +0100

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,440 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hola 👋.",
+  title2: "Titi",
+  logo_name: "TITI.()",
+  nickname: "ertiti",
+  full_name: "Adrián Prieto Vuosalmi",
+  subTitle:
+    "Pentester & Programador",
+  resumeLink:
+    "https://docs.google.com/document/d/1A_jWRG74Rst427tg1izLa6vRXclS9_9F856jWJPJlDY/edit?usp=sharing",
+  mail: "mailto:vatitiprieto11@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/ertiti11",
+  linkedin: "https://www.linkedin.com/in/titiprieto/",
+  gmail: "vatitiprieto11@gmail.com",
+  // gitlab: "https://gitlab.com/harikanani",
+  facebook: "https://www.facebook.com/titiprieto11/",
+  twitter: "https://twitter.com/ertiti11",
+  instagram: "https://www.instagram.com/11titiprieto/",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Hacking",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications",
+        "⚡ Building responsive website front end using ReactJS",
+        "⚡ Developing mobile applications using Flutter and Android",
+        "⚡ Creating application backend in Node, Express & Flask",
+        "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+      ],
+      softwareSkills: [
+        {
+          skillName: "linux",
+          fontAwesomeClassname: "simple-icons:linux",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "networking",
+          fontAwesomeClassname: "simple-icons:networking",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Heroku",
+          fontAwesomeClassname: "simple-icons:heroku",
+          style: {
+            color: "#6863A6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        {
+          skillName: "PostgreSQL",
+          fontAwesomeClassname: "simple-icons:postgresql",
+          style: {
+            color: "#336791",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+        {
+          skillName: "GitHub Actions",
+          fontAwesomeClassname: "simple-icons:githubactions",
+          style: {
+            color: "#5b77ef",
+          },
+        },
+      ],
+    },
+    {
+      title: "Programador",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experiencia en proyectos propios",
+        "⚡ Experiencia en manteniento sitios web",
+        
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "PHP",
+          fontAwesomeClassname: "simple-icons:php",
+          style: {
+            color: "#7377AD",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        // {
+        //   skillName: "GraphQL",
+        //   fontAwesomeClassname: "simple-icons:graphql",
+        //   style: {
+        //     color: "#DE33A6",
+        //   },
+        // },
+        // {
+        //   skillName: "Visual Basic",
+        //   fontAwesomeClassname: "simple-icons:dot-net",
+        //   style: {
+        //     color: "#029FCE",
+        //   },
+        // },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+        {
+          skillName: "jQuery",
+          fontAwesomeClassname: "simple-icons:jquery",
+          style: {
+            color: "#0865A6",
+          },
+        },
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#207297",
+          },
+        },
+        {
+          skillName: "Apache",
+          fontAwesomeClassname: "simple-icons:apache",
+          style: {
+            color: "#CA1A22",
+          },
+        },
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            color: "#3776AB",
+          },
+        },
+      ],
+      
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Escuela internacional de posgrados",
+      subtitle: "Máster en Dirección de Ciberseguridad, Hacking Ético y Seguridad Ofensiva",
+      logo_path: "EIP.png",
+      alt_name: "Escuela internacional de posgrados",
+      duration: "2021 - Present",
+      descriptions: [
+        "⚡ I'm currently pursuing my bachelors in Information Technology."
+        // "⚡ I have studied core subjects like Data Structures, DBMS, Networking, Security, etc.",
+        // "⚡ I have also completed various online courses for Backend , Web , Mobile App Development, etc.",
+        // "⚡ I have implemented several projects based on what I've leart under my Computer Engineering course. ",
+      ],
+      website_link: "http://www.ssgec.ac.in/",
+    },
+    {
+      title: "Dr. S. & S. S. Ghandhy College Of Engineering & Technology",
+      subtitle: "Diploma in Information Technology",
+      logo_path: "iesfuengirola1.png",
+      alt_name: "SSGC",
+      duration: "2018 - 2021",
+      descriptions: [
+        "⚡ I have studied core subjects like Data Structures, DBMS, Networking, Security, etc.",
+        "⚡ I have also completed various online courses for Backend , Web , Mobile App Development, etc.",
+        "⚡ I have implemented several projects based on what I've leart under my Computer Engineering course. ",
+      ],
+      website_link: "http://www.ssgc.cteguj.in/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "M0001: MongoDB Basics",
+      subtitle: "MongoDB University",
+      logo_path: "mongo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/12se6bMep48WXA1PlizTBAgCnW0GXM6wq/view?usp=sharing",
+      alt_name: "MongoDB University",
+      // color_code: "#2AAFED",
+      color_code: "#47A048",
+    },
+    {
+      title: "Hackathon",
+      subtitle: "Hack The Mountains",
+      logo_path: "hackathon1.svg",
+      certificate_link:
+        "https://drive.google.com/file/d/1Y-HT1flWDfeyfnfqVr3ubNgGIcnuUEYt/view?usp=sharing",
+      alt_name: "hackathon",
+      color_code: "#E2405F",
+    },
+    {
+      title: "A300: Atlas Security",
+      subtitle: "MongoDB University",
+      logo_path: "mongo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/12u_tkvhgB-K2TIm-RDdJOwYbQ9ccqiqA/view?usp=sharing",
+      alt_name: "MongoDB University",
+      // color_code: "#F6B808",
+      color_code: "#47A048",
+    },
+    {
+      title: "Cyber Security & Cyber Forensics",
+      subtitle: "Workshop at IIT Bombay",
+      logo_path: "iit.png",
+      certificate_link:
+        "https://drive.google.com/file/d/12uAdjQC5LfrB1ODdxqIY181ugyyQhl-I/view?usp=sharing",
+      alt_name: "Workshop",
+      color_code: "#2AAFED",
+    },
+    {
+      title: "MLH Local Hack Day: Build",
+      subtitle: "Major League Hacking",
+      logo_path: "mlh-logo.svg",
+      certificate_link:
+        "https://drive.google.com/file/d/1ws7g2Oepu76vDFir6SvR_emFAMSkB2ZQ/view?usp=sharing",
+      alt_name: "Google",
+      color_code: "#fe0037",
+    },
+    {
+      title: "Hack20",
+      subtitle: "Flutter International Hackathon",
+      logo_path: "flutter.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1JTd8pVlHbP0RVQYtfa4OZgJYGfdiCwM6/view?usp=sharing",
+      alt_name: "Flutter International Hackathon",
+      color_code: "#2AAFED",
+    },
+    {
+      title: "Postman Student Expert",
+      subtitle: "Postman",
+      logo_path: "postman.png",
+      certificate_link:
+        "https://badgr.com/public/assertions/h2qevHs4SlyoHErJQ0mn2g",
+      alt_name: "Postman",
+      // color_code: "#f36c3d",
+      color_code: "#fffbf3",
+    },
+    // color_code: "#8C151599",
+    // color_code: "#7A7A7A",
+    // color_code: "#0C9D5899",
+    // color_code: "#C5E2EE",
+    // color_code: "#ffc475",
+    // color_code: "#g",
+    // color_code: "#ffbfae",
+    // color_code: "#fffbf3",
+    // color_code: "#b190b0",
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I've completed one internship. I've mostly done projects on my own and I am actively looking for internships. I love organizing workshops to share my knowledge with others.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work Experience",
+      experiences: [
+        {
+          title: "administrador de sistema",
+          company: "Golf in spain",
+          company_url: "https://golfinspain.com/",
+          logo_path: "gis-logo.svg",
+          duration: "julio 2021 - septiembre 2021",
+          location: "Office",
+          description:
+          `administracion de servidores de Golf in spain y mantenimiento de sitio web.
+          `,
+            // "I worked on the Dashboard project which helps users track their activities while using Walo Application. I also worked on Ocean Inventory Application and it's Admin panel Backend as well as on Ocean Inventory Admin Front-end using React and also worked on Walo Admin Backend.",
+          color: "#0071C5",
+        },
+        
+      ],
+    },
+    // {
+    //   title: "Volunteerships",
+    //   experiences: [
+      
+    //     {
+    //       title: "Campus Hustler",
+    //       company: "Skillenza",
+    //       company_url: "https://skillenza.com/",
+    //       logo_path: "skillenza.png",
+    //       duration: "Feb 2021 - Present",
+    //       location: "Work from Home",
+    //       description: "Spread Awareness of new Technologies and new Opportunities to Students and Grow Skillenza Community.",
+    //       color: "#196acf",
+    //     },
+        
+    //   ],
+    // },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects make use of a vast variety of latest technology tools. My best experience is to create NodeJS Backend Projects, Python Scripts, and React Project. Below are some of my projects. Note that not all of the mentioned projects are on GitHub yet.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contáctame",
+    profile_image_path: "yo.png",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
+    link: "https://linkedin.com/titiprieto11",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "AutomateInstaPyBot",
+      url: "https://github.com/harikanani/AutomateInstaPyBot",
+      description: "This is Instagram Bot. This will login to your Instagram account. Follow Users, Unfollow Users, Remove Profile Photo",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+    
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
